dd2a25c911d48b5292e69a8a77b99c557487faef
Dylan Neve <dylanneve1@gmail.com>
2021-12-05 18:40:00 +0000

    universal9810: Add initial vendor SEPOLICY

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -137,6 +137,7 @@ BOARD_PROPERTY_OVERRIDES_SPLIT_ENABLED := true
 
 # SELinux
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(COMMON_PATH)/sepolicy/private
+BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
 
 # Inherit from the proprietary version
 -include vendor/samsung/universal9810-common/BoardConfigVendor.mk
