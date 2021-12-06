70067e897cb30bbb34f38775757dbcde917c8c32
Dylan Neve <dylanneve1@gmail.com>
2021-12-06 09:47:01 +0000

    universal9810: Build Neural Networks + add 1.0 to manifest

--- universal9810-common.mk
+++ universal9810-common.mk
@@ -133,6 +133,11 @@ PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0-service \
     libbt-vendor:64
 
+# Neural Networks
+PRODUCT_PACKAGES += \
+    android.hardware.neuralnetworks@1.2 \
+    android.hardware.neuralnetworks@1.0
+
 # Init
 PRODUCT_PACKAGES += \
     fstab.zram \
