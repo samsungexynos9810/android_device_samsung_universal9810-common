947e63dd8ecc2c93c71e1a03557110ab76877412
Dylan Neve <dylanneve1@gmail.com>
2021-12-08 13:56:47 +0000

    Build more required audio packages
    - Fix error:
    07-13 00:20:10.980     0     0 I init    : Command 'restart vendor.audio-hal-4-0-msd' action=onrestart (<Service 'audioserver' onrestart>:2) took 0ms and failed: service vendor.audio-hal-4-0-msd not found
    07-13 00:20:10.980     0     0 I init    : Command 'restart vendor.audio-hal-2-0' action=onrestart (<Service 'audioserver' onrestart>:3) took 0ms and failed: service vendor.audio-hal-2-0 not found
    07-13 00:20:10.980     0     0 I init    : Command 'restart audio-hal-2-0' action=onrestart (<Service 'audioserver' onrestart>:4) took 0ms and failed: service audio-hal-2-0 not found

--- universal9810-common.mk
+++ universal9810-common.mk
@@ -23,6 +23,8 @@ TARGET_INCLUDE_LIVE_WALLPAPERS := true
 
 # Audio
 PRODUCT_PACKAGES += \
+    android.hardware.audio@2.0-service \
+    android.hardware.audio4.0-service \
     android.hardware.audio.service \
     android.hardware.audio@5.0-impl \
     android.hardware.audio.effect@5.0-impl \
