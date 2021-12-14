5725a5e60691cc60c765666059a33811cb0ccf4f
Dylan Neve <dylanneve1@gmail.com>
2021-12-14 21:43:32 +0000

    universal9810: Cleanup Keymaster Products

--- universal9810-common.mk
+++ universal9810-common.mk
@@ -126,9 +126,7 @@ PRODUCT_PACKAGES += \
 
 # Keymaster
 PRODUCT_PACKAGES += \
-    android.hardware.keymaster@3.0 \
-    android.hardware.keymaster@3.0-service \
-    android.hardware.keymaster@3.0-impl
+    android.hardware.keymaster@3.0-service.samsung
 
 PRODUCT_COPY_FILES += \
     prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-core/libkeymaster_portable.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libkeymaster_portable-v29.so \
