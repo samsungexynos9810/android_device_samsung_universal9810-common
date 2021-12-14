5a193c36471e8f0a10dca628a3c0ae7803f55cb5
Alexander Koskovich <zvnexus@outlook.com>
2021-12-14 16:35:08 +0000

    universal9810: Disable the usage of ConfigStore
    * Also remove manifest entry

--- universal9810-common.mk
+++ universal9810-common.mk
@@ -64,8 +64,7 @@ PRODUCT_PACKAGES += \
 
 # Configstore
 PRODUCT_PACKAGES += \
-    android.hardware.configstore@1.0-impl \
-    android.hardware.configstore@1.0-service
+    disable_configstore
 
 # DRM
 PRODUCT_PACKAGES += \
