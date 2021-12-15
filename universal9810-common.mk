21619e4cd9a492f7d741492e2e6672914eed5c5a
Tim Zimmermann <tim@linux4.de>
2021-12-15 10:05:58 +0000

    universal9810: Uprev fingerprint service to 2.3

--- universal9810-common.mk
+++ universal9810-common.mk
@@ -73,7 +73,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-service.samsung
+    android.hardware.biometrics.fingerprint@2.3-service.samsung
 
 # Flat device tree for boot image
 PRODUCT_HOST_PACKAGES += \
