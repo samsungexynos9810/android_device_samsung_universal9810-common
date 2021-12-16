60de255558e2604c78f3d6bfbfbeb8f341080521
Dylan Neve <dylanneve1@gmail.com>
2021-12-16 21:01:28 +0000

    universal9810: Disable ConfigStore properly

--- universal9810-common.mk
+++ universal9810-common.mk
@@ -56,8 +56,7 @@ PRODUCT_PACKAGES += \
 
 # Configstore
 PRODUCT_PACKAGES += \
-    android.hardware.configstore@1.0-service \
-    android.hardware.configstore@1.1-service
+    disable_configstore
 
 # DRM
 PRODUCT_PACKAGES += \
