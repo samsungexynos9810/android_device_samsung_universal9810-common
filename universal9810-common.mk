a46db47f5fd350cafeea3420d89676b4c48e0254
Dylan Neve <dylanneve1@gmail.com>
2021-12-16 17:10:00 +0000

    universal9810: Add call to build audio@2.0-service & audio@4.0-service
    - Fix error:
    07-13 00:20:10.980     0     0 I init    : Command 'restart vendor.audio-hal-4-0-msd' action=onrestart (<Service 'audioserver' onrestart>:2) took 0ms and failed: service vendor.audio-hal-4-0-msd not found
    07-13 00:20:10.980     0     0 I init    : Command 'restart vendor.audio-hal-2-0' action=onrestart (<Service 'audioserver' onrestart>:3) took 0ms and failed: service vendor.audio-hal-2-0 not found
    07-13 00:20:10.980     0     0 I init    : Command 'restart audio-hal-2-0' action=onrestart (<Service 'audioserver' onrestart>:4) took 0ms and failed: service audio-hal-2-0 not found

--- universal9810-common.mk
+++ universal9810-common.mk
@@ -19,6 +19,8 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@7.0-impl \
     android.hardware.audio.service \
     android.hardware.audio.effect@7.0-impl \
+    android.hardware.audio@2.0-service \
+    android.hardware.audio4.0-service \
     libtinycompress \
     audio.r_submix.default \
     audio.usb.default
