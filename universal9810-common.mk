74f842d6d5e5dc9044a9baacf2bfd8fffbfcf619
Dylan Neve <dylanneve1@gmail.com>
2021-12-16 15:55:07 +0000

    Revert "universal9810: Clean-up and uprev Graphics Mapper to 2.1"
    This reverts commit 01ce62bf19dd2bf228bb1359ec7b9d8b5e0d9f5e.

--- universal9810-common.mk
+++ universal9810-common.mk
@@ -80,11 +80,13 @@ PRODUCT_PACKAGES += \
 # Graphics
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
+    android.hardware.graphics.allocator@3.0-impl \
+    android.hardware.graphics.allocator@4.0-impl \
     android.hardware.graphics.allocator@2.0-service \
-    android.hardware.graphics.composer@2.2-impl \
     android.hardware.graphics.composer@2.2-service \
     android.hardware.graphics.mapper@2.0-impl \
-    android.hardware.graphics.mapper@2.0-impl-2.1 \
+    android.hardware.graphics.mapper@3.0-impl \
+    android.hardware.graphics.mapper@4.0-impl \
     libgui_vendor
 
 # Health
