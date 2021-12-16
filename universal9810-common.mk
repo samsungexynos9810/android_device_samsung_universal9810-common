86c7545dd04da2ae465d21b8f3fee9b71b6ba32d
Dylan Neve <dylanneve1@gmail.com>
2021-12-16 23:30:02 +0000

    universal9810: Build OSS secril_config_svc
    * This can now be built from source

--- universal9810-common.mk
+++ universal9810-common.mk
@@ -316,6 +316,7 @@ PRODUCT_PACKAGES += \
 
 # RIL
 PRODUCT_PACKAGES += \
+    secril_config_svc \
     android.hardware.radio@1.4.vendor \
     android.hardware.radio.config@1.2.vendor \
     android.hardware.radio.deprecated@1.0.vendor
