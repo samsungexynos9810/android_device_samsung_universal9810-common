b2514759da8d27bd766c1314fee5127022bc120b
Dylan Neve <dylanneve1@gmail.com>
2021-12-17 18:45:24 +0000

    universal9810: Drop depreciated flag

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -113,7 +113,6 @@ WIFI_DRIVER_FW_PATH_STA          := "/vendor/etc/wifi/bcmdhd_sta.bin"
 WIFI_DRIVER_FW_PATH_AP           := "/vendor/etc/wifi/bcmdhd_apsta.bin"
 WIFI_BAND                        := 802_11_ABG
 WIFI_HIDL_FEATURE_DUAL_INTERFACE := true
-WIFI_HIDL_FEATURE_DISABLE_AP_MAC_RANDOMIZATION := false
 WIFI_RESET_IFACE_AFTER_MAC_CHANGE := true
 WIFI_HIDL_UNIFIED_SUPPLICANT_SERVICE_RC_ENTRY := true
 PRODUCT_CFI_INCLUDE_PATHS += hardware/samsung_slsi/scsc_wifibt/wpa_supplicant_lib
