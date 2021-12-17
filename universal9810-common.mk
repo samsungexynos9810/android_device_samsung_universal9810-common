f8a02a5a5488a35995425111008a49fb59c1cc71
Dylan Neve <dylanneve1@gmail.com>
2021-12-17 17:48:38 +0000

    universal9810: Clean-up WiFi Packages

--- universal9810-common.mk
+++ universal9810-common.mk
@@ -306,12 +306,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     wifi_sec.rc \
     hostapd \
-    wificond \
-    wifilogd \
-    wlutil \
-    libwpa_client \
     wpa_supplicant \
-    wpa_supplicant.conf \
     android.hardware.wifi@1.0 \
     android.hardware.wifi@1.0-impl \
     android.hardware.wifi@1.0-service
