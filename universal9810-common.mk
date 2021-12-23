2ec182a1d8ca201bce1faa77d69235edfdf5ef9e
Dylan Neve <dylanneve1@gmail.com>
2021-12-23 23:18:25 +0000

    universal9810: Clean-up Audio packages

--- universal9810-common.mk
+++ universal9810-common.mk
@@ -19,12 +19,8 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.effect@7.0-impl \
     android.hardware.audio@7.0-impl \
     android.hardware.audio.service \
-    android.hardware.bluetooth.audio@2.0-impl:32 \
     android.hardware.soundtrigger@2.3-impl \
-    android.hardware.audio@2.0-service \
-    android.hardware.audio4.0-service \
     libtinycompress \
-    audio.primary.default \
     audio.r_submix.default \
     audio.usb.default
 
@@ -39,15 +35,9 @@ PRODUCT_COPY_FILES += \
 
 # Bluetooth
 PRODUCT_PACKAGES += \
-    android.hardware.bluetooth.audio@2.1-impl \
-    android.hardware.bluetooth.audio@2.0-impl \
     android.hardware.bluetooth@1.0-impl \
     android.hardware.bluetooth@1.0-service \
-    android.hardware.bluetooth.a2dp@1.0-impl \
-    android.hardware.bluetooth.a2dp@1.0-service \
-    audio.bluetooth.default \
     audio.a2dp.default \
-    libldacBT_bco \
     libbt-vendor:64
 
 # Boot animation
