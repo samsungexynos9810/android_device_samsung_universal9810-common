ae0659a42528894f8a1e1dfdb8874bb78b8cb0e1
Dylan Neve <dylanneve1@gmail.com>
2022-01-01 17:37:48 +0000

    universal9810: Update DRM packages

--- universal9810-common.mk
+++ universal9810-common.mk
@@ -83,13 +83,11 @@ PRODUCT_COPY_FILES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.0 \
-    android.hardware.drm@1.1 \
-    libfwdlockengine \
-    libdrmclearkeyplugin \
+    android.hardware.drm@1.0-impl \
     android.hardware.drm@1.0-service \
     android.hardware.drm@1.4-service.clearkey \
-    android.hardware.drm@1.0-impl \
+    android.hardware.drm@1.1.vendor \
+    android.hardware.drm@1.2.vendor \
     android.hardware.drm@1.3.vendor
 
 # Fingerprint
