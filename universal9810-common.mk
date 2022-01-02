242501daf3a4ff215f971009e43262d12ab9d632
Dylan Neve <dylanneve1@gmail.com>
2022-01-02 01:00:52 +0000

    universal9810: DRM: Also build 1.0 vender variant

--- universal9810-common.mk
+++ universal9810-common.mk
@@ -80,6 +80,7 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
     android.hardware.drm@1.0-service \
     android.hardware.drm@1.4-service.clearkey \
+    android.hardware.drm@1.0.vendor \
     android.hardware.drm@1.1.vendor \
     android.hardware.drm@1.2.vendor \
     android.hardware.drm@1.3.vendor
