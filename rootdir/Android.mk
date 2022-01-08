e354ffba88c178cccdb7b9bbb748846d9bce18c9
Dylan Neve <dylanneve1@gmail.com>
2022-01-08 02:04:08 +0000

    universal9810: rootdir: Remove duplicate definition

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -57,14 +57,6 @@ LOCAL_MODULE_PATH       := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE		    := init.samsung.rc
-LOCAL_MODULE_TAGS	    := optional
-LOCAL_MODULE_CLASS	    := ETC
-LOCAL_SRC_FILES		    := etc/init.samsung.rc
-LOCAL_MODULE_PATH	    := $(TARGET_ROOT_OUT)
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE		    := init.samsungexynos9810.rc
 LOCAL_MODULE_TAGS	    := optional
 LOCAL_MODULE_CLASS	    := ETC
