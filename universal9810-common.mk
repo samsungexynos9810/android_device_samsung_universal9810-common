bf47d47eb27da9a9f2cf145ccba0db13f6cc107c
Dylan Neve <dylanneve1@gmail.com>
2022-01-15 01:46:04 +0000

    universal9810: power: Update build package name

--- universal9810-common.mk
+++ universal9810-common.mk
@@ -274,7 +274,7 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.0-service.exynos \
+    android.hardware.power@1.0-service.universal9810 \
     android.hardware.power.stats@1.0-service.mock
 
 # Ramdisk
