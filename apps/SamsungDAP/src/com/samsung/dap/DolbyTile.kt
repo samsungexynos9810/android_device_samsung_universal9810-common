281a998f4255ffc48a0627359c71f7295fda5c97
Dylan Neve <dylanneve1@gmail.com>
2022-01-21 19:49:23 +0000

    SamsungDAP: Clean-up

--- apps/SamsungDAP/src/com/samsung/dap/DolbyTile.kt
+++ apps/SamsungDAP/src/com/samsung/dap/DolbyTile.kt
@@ -12,7 +12,6 @@ import android.content.Context
 import android.content.Intent
 import android.media.audiofx.AudioEffect
 import android.util.Log
-import com.samsung.dap.DolbyCore
 
 import java.util.*
 
