b1c9b3a1cf0af0e64a5d17b7e1999a7247551298
Dylan Neve <dylanneve1@gmail.com>
2022-01-21 12:31:20 +0000

    SamsungDAP: Redefine audioEffect for Tile Service instead

--- apps/SamsungDAP/src/com/samsung/dap/BootCompletedReceiver.kt
+++ apps/SamsungDAP/src/com/samsung/dap/BootCompletedReceiver.kt
@@ -16,7 +16,7 @@ import android.util.Log
 import java.util.*
 
 class BootCompletedReceiver : BroadcastReceiver() {
-    public val audioEffect = AudioEffect(
+    private val audioEffect = AudioEffect(
         EFFECT_TYPE_DAP, AudioEffect.EFFECT_TYPE_NULL, 0, 0
     )
 
@@ -43,6 +43,10 @@ class BootCompletedReceiver : BroadcastReceiver() {
 }
 
 class DolbyTile : TileService() {
+    private val audioEffect = AudioEffect(
+        EFFECT_TYPE_DAP, AudioEffect.EFFECT_TYPE_NULL, 0, 0
+    )
+
     override fun onStartListening() {
         updateTile()
     }
