90a56d958729fc32af8442e4f3752b913ec28e80
Rob Burton <synt4x.93@gmail.com>
2022-01-22 21:23:58 +0000

    universal9810: recovery: Update FSTAB and ship

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -127,6 +127,9 @@ TARGET_USES_HWC2 := true
 NUM_FRAMEBUFFER_SURFACE_BUFFERS := 3
 BACKLIGHT_PATH := "/sys/class/backlight/panel/brightness"
 
+# Recovery
+TARGET_USES_AOSP_RECOVERY := true
+
 # Security
 BOOT_SECURITY_PATCH = $(PLATFORM_SECURITY_PATCH)
 
