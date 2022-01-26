b06d13c11661a172c7d15565e89dd71ec80300f7
Michael Benedict <michaelbt@live.com>
2022-01-26 01:31:24 +0000

    universal9810: BoardConfig: Build updateable apex

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -6,6 +6,9 @@ COMMON_PATH := device/samsung/universal9810-common
 # Apex
 DEXPREOPT_GENERATE_APEX_IMAGE := true
 
+# Apex Updateable Inheritance
+$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+
 # Architecture
 TARGET_ARCH         := arm64
 TARGET_ARCH_VARIANT := armv8-a
