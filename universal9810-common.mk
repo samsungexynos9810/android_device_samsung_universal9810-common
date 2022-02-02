a90fd3d16f208df0b1627cb8e4624a359acc7e26
Roberto Sartori <roberto.sartori.android@gmail.com>
2022-02-02 01:53:29 +0000

    universal9810: apex: Disable compression

--- universal9810-common.mk
+++ universal9810-common.mk
@@ -3,6 +3,9 @@
 
 # Apex
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+# Disable APEX compression
+# Keep this after including updatable_apex.mk
+PRODUCT_COMPRESSED_APEX := false
 
 # AptX
 PRODUCT_COPY_FILES += \
