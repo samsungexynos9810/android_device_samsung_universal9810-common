53b8e84302965a6b91f9fbc8ec09122de5b4b0bf
Dylan Neve <dylanneve1@gmail.com>
2022-02-03 00:48:22 +0000

    universal9810: drm: Clean-up packages and manifest entry

--- universal9810-common.mk
+++ universal9810-common.mk
@@ -84,16 +84,8 @@ PRODUCT_PACKAGES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.0 \
-    android.hardware.drm@1.1 \
-    android.hardware.drm@1.2 \
-    android.hardware.drm@1.3 \
-    android.hardware.drm@1.4 \
-    android.hardware.drm@1.0.vendor \
-    android.hardware.drm@1.1.vendor \
-    android.hardware.drm@1.2.vendor \
-    android.hardware.drm@1.3.vendor \
-    android.hardware.drm@1.4.vendor \
+    android.hardware.drm@1.3.vendor:32 \
+    android.hardware.drm@1.4.vendor:32 \
     android.hardware.drm@1.4-service.clearkey \
     libdrm.vendor:64
 
