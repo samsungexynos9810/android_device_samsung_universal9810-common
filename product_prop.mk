80103759944dd8d2dfd16c22dd11954ef90d8f55
Dylan Neve <dylanneve1@gmail.com>
2022-02-06 01:49:08 +0000

    universal9810: media: Enable C2
    This breaks third party media playback

--- product_prop.mk
+++ product_prop.mk
@@ -55,7 +55,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Codec2
 PRODUCT_PROPERTY_OVERRIDES += \
-    debug.stagefright.ccodec=0 \
     debug.stagefright.omx_default_rank=0 \
     debug.stagefright.omx_default_rank.sw-audio=1
 
